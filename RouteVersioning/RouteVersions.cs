9e4297279299bc9c303f044bb25d22397c5e513f
Ravindu Liyanapathirana <ravinduwu@outlook.com>
2025-01-21 21:56:10 +1100

    Make RouteVersions ctor internal, allow construction only via RouteVersionBuilder

--- RouteVersioning/RouteVersions.cs
+++ RouteVersioning/RouteVersions.cs
@@ -7,7 +7,7 @@ using System.Linq;
 public class RouteVersions<T>
 	where T : struct, IComparable
 {
-	public RouteVersions(ISet<T> set, Func<T, string> prefix)
+	internal RouteVersions(ISet<T> set, Func<T, string> prefix)
 	{
 		Set = set is IReadOnlySet<T> s ? s : set.ToHashSet();
 		Prefix = prefix;
