29f2470996fe6cad4fe8eb13398f82878ff9c996
Ravindu Liyanapathirana <ravinduwu@outlook.com>
2025-01-23 15:27:31 +1100

    Make RouteVersionMetadataBuilder ctor internal

--- RouteVersioning/RouteVersionMetadataBuilder.cs
+++ RouteVersioning/RouteVersionMetadataBuilder.cs
@@ -4,10 +4,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-public class RouteVersionMetadataBuilder<T>(T version)
+public class RouteVersionMetadataBuilder<T>
 	where T : struct
 {
+	private readonly T version;
 	private readonly Dictionary<Type, List<object>> features = [];
+
+	internal RouteVersionMetadataBuilder(T version)
+	{
+		this.version = version;
+	}
 
 	public RouteVersionMetadataBuilder<T> WithFeature<F>(F feature)
 		where F : notnull
